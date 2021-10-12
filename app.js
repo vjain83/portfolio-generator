f8d89e86d8378256103ed11651bcd4322f802221
Varsha Jain <csvarshajain@gmail.com>
2021-10-12 14:16:22 -0400

    generate html file

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-var profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-profileDataArr.forEach(profileItem => console.log(profileItem));
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
 
-printProfileData(profileDataArgs);
+
+fs.writeFile(`./index.html`, generatePage(name, github), err => {
+    if (err) throw err;
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
